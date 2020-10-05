45473a72df3ff0fdbb19206d725a39010c2aace0
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-10-05 15:02:37 -0400

    updated deploy files

--- back_end/config/deploy.rb
+++ back_end/config/deploy.rb
@@ -1,14 +1,19 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "table_host"
+set :repo_url, "https://github.com/bengarlock/table_host.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/projects/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+set :keep_releases, 5
+
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
